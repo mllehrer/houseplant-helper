2a133c64d8af336c05dd5a1b8305e751a149210a
mllehrer <mia.lehrer@gmail.com>
2020-10-15 13:31:40 -0700

    fill in a little more

--- src/houseplant_helper/schedule.clj
+++ src/houseplant_helper/schedule.clj
@@ -1,24 +1,58 @@
 (ns houseplant-helper.schedule
   (:require [houseplant-helper.client :as client]))
 
-;; n days per schedule
-;; Generate data structure that maps each day to the plants that need to be watered
-;; Every plant has name and cadence
-;; divide n by cadence to determine schedule
-;; output csv?
-
 (defn get-day-numbers
   "gets days of schedule n on which plant with cadence c should be watered"
   [n c]
-  ;;use filter to return only members of collection for which something is true
-  (filter
-   ;; create an anonymous function to check each element in a collection
-   #(zero? (mod % c))
-   ;; range goes from 0 to n-1 so we want to increment n by 1
-   (range (inc n))))
+  ;;return a set because we want to use "contains?" later
+  (set
+   ;;use filter to return only members of collection for which something is true
+   (filter
+    ;; create an anonymous function to check each element in a collection
+    #(zero? (mod % c))
+    ;;range goes from 0 to n-1 so we want to increment n by 1
+    ;;the zero day will become day 1 on our schedule.
+    ;;(zero? (mod 0 n)) is always true. All plants are watered on day 1.
+    (range (inc n)))))
+
+(defn plant->days
+  "map each plant to the days it needs to be watered"
+  [plants]
+  )
+
+(defn water-on-day?
+  "checks to see if you water a given plant on a given day"
+  [day plant]
+  (contains? (:day-numbers plant) day))
 
 (defn generate-schedule
   "creates a data structure that maps the days provided to the plants that need to be watered"
   [{:keys [days plants]}]
-  (let [plant-data (client/get plants)
-        by-cadence (group-by :cadence plant-data)]))
+  (let [schedule-days ;;make a collection of day numbers in the schedule
+        ;;zipmap returns a map of a collection to another collection
+        (zipmap
+         ;;use "rest" function to remove the first element in a collection.
+         ;;in this case, calendars don't have day 0 so neither should our schedule.
+         (range
+          ;;range goes from start value to n-1 so we want to increment n by 1
+          1
+          (inc days))
+         ;;generate a collection of empty strings with one empty string per day in the schedule
+         (repeat ""))
+        ;;retrieve the plant data from the API
+        plant-data (client/get plants)
+        ;;use "map" to apply the same function to each element in a collection and return a new collection
+        ;;in this case we are going to run our get-day-numbers function to add the days each plant should
+        ;;be watered to the data about the plant
+        with-day-numbers (map
+                          ;;assoc adds a new key and value to a map or a new value to an existing key
+                          #(assoc % :day-numbers
+                                  (get-day-numbers days (:cadence %)))
+                          plant-data)]
+    ;;now the magic happens! We're going to make a new map where every day of the schedule is mapped
+    ;;to a list of plants that need to be watered on that day
+    (for [day schedule-days]
+      (if (contains? )))
+    ))
+
+
